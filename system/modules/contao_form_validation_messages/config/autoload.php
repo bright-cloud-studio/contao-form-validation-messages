51948738c29a0a5e7d1b70a42a2f41a526a2508c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-25 15:25:06 -0400

    Create autoload.php

---
+++ system/modules/contao_form_validation_messages/config/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+// Register Classes
+ClassLoader::addClasses(array
+(
+    'Bcs\Backend\addValidationMessages'         => 'system/modules/contao_form_validation_messages/library/Bcs/Backend/addValidationMessages.php'
+));
